4364818924d8fdce75b358ea029bca971b35988e
alenzo <alenzo.amico@gmail.com>
2024-03-21 15:54:52 +0100

    server modif

--- Server/server.go
+++ Server/server.go
@@ -1,16 +1,20 @@
 package Server
 
 import (
-	"Groupie-Tracker/Internal"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
+
+	"Groupie-Tracker/Internal"
 )
 
 var tmpl *template.Template
 var tmpl_map *template.Template
+var tmpl_groupe *template.Template
 
 func StartServer() {
 	var err error
@@ -21,6 +25,11 @@ func StartServer() {
 	}
 
 	tmpl_map, err = template.New("map").ParseFiles("Web/HTML/map.html")
+	if err != nil {
+		panic(err)
+	}
+
+	tmpl_groupe, err = template.New("groupe").ParseFiles("Web/HTML/groupe.html")
 	if err != nil {
 		panic(err)
 	}
@@ -48,6 +57,21 @@ func StartServer() {
 		} else {
 			fileServer.ServeHTTP(w, r)
 		}
+	})
+
+	http.HandleFunc("/group/", func(w http.ResponseWriter, r *http.Request) {
+		// Extraire groupID de l'URL
+		groupID := strings.TrimPrefix(r.URL.Path, "/group/")
+		// Trouver le groupe correspondant dans Groups
+		for _, group := range Internal.Artists {
+			if strconv.Itoa(group.ID) == groupID {
+				// Afficher le template avec les détails du groupe
+				tmpl_groupe.ExecuteTemplate(w, "groupe", group)
+				return
+			}
+		}
+		// Gérer le cas où le groupe n'est pas trouvé
+		http.NotFound(w, r)
 	})
 
 	http.HandleFunc("/updateFilters", func(w http.ResponseWriter, r *http.Request) {
