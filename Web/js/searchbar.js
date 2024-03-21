eb99ec66705f1fa1c4d979b2eda428577ac367df
MelfStudent <melvin.favaro@ynov.com>
2024-03-21 14:50:04 +0100

    Fix dynamic search bar

--- Web/js/searchbar.js
+++ Web/js/searchbar.js
@@ -2,10 +2,13 @@ function searchArtist() {
     var input = document.getElementsByClassName('input')[0].value.toLowerCase();
     var cards = document.querySelectorAll('.flip-card');
 
+    var found = false;
+
     for (var i = 0; i < cards.length; i++) {
         var artistName = cards[i].getAttribute('data-artist').toLowerCase();
         if (artistName.includes(input)) {
             cards[i].style.display = '';
+            found = true;
         } else {
             cards[i].style.display = 'none';
         }
