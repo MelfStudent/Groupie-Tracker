bdd7bcdf748b522bd951e92064e4c5ad44b009dd
MelfStudent <melvin.favaro@ynov.com>
2024-03-22 13:48:53 +0100

    Fix error filter number of members

--- Internal/filterPassed.go
+++ Internal/filterPassed.go
@@ -1,43 +1,59 @@
 package Internal
 
 import (
+	"fmt"
 	"strings"
 )
 
 // FilterPassed Filters API data with the desired filters
 func FilterPassed(artist Artist, filters FormValues) bool {
+	//fmt.Println(artist)
+	var result = false
 	if filters.MinCreationDate != 0 && filters.MaxCreationDate != 0 {
 		if artist.CreationDate > filters.MinCreationDate && artist.CreationDate < filters.MaxCreationDate {
-			return true
+			result = true
+		} else {
+			result = false
 		}
 	}
 
 	if filters.MinFirstAlbum != 0 && filters.MaxFirstAlbum != 0 {
 		if filters.MinFirstAlbum > filters.MinFirstAlbum && filters.MaxFirstAlbum < filters.MaxFirstAlbum {
-			return true
+			result = true
+		} else {
+			result = false
 		}
 	}
 
+	var number = [7]bool{false, false, false, false, false, false, false}
 	for i := range filters.NumberMembers {
 		if filters.NumberMembers[i] != 0 {
 			if len(artist.Members) == filters.NumberMembers[i] {
-				return true
+				number[i] = true
+			} else {
+				number[i] = false
 			}
 		}
+	}
+	if number[0] == true || number[1] == true {
+		result = true
+	} else {
+		result = false
 	}
 
 	if filters.LocationConcert != "" {
 		for i := range artist.Locations {
 			research := WordProcessing(filters.LocationConcert)
 			for j := 0; j < len(research); j++ {
+				fmt.Println(strings.ToLower(artist.Locations[i]), strings.ToLower(research[j]))
 				if strings.Contains(strings.ToLower(artist.Locations[i]), strings.ToLower(research[j])) {
-					return true
+					result = true
 				}
 			}
 		}
 	}
 
-	return false
+	return result
 }
 
 // WordProcessing removed all non-important spaces
